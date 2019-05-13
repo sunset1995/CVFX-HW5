45b3af6a1446d8ba387dfc50d2f3ef8f06bcfb65
ChiWeiHsiao <kiwi010379@gmail.com>
2019-05-14 01:41:58 +0800

    add crop

--- resize_all.py
+++ resize_all.py
@@ -6,6 +6,8 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument('--img_format', required=True)
 parser.add_argument('--h', default=1024, type=int)
 parser.add_argument('--w', default=768, type=int)
+parser.add_argument('--crop', action='store_true')
+
 args = parser.parse_args()
 
 # Read all images
@@ -13,7 +15,17 @@ img_paths = []
 for i in range(100000):
     path = args.img_format % i
     if os.path.isfile(path):
-        print('Processing', path)
-        Image.open(path).resize((args.w, args.h), Image.LANCZOS).save(path)
+        if not args.crop:
+            print('Processing', path)
+            Image.open(path).resize((args.w, args.h), Image.LANCZOS).save(path)
+        else:
+            im = Image.open(path)
+            width, height = im.size
+            left = (width - args.w)/2
+            top = (height - args.h)/2
+            right = (width + args.w)/2
+            bottom = (height + args.h)/2
+            box = (left, top, right, bottom)
+            im.crop(box).save(path)
     else:
         break
